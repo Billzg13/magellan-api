af4bd1e2946ffd627af23b92f6489c1ffee2600e
zgantzouris <billzg@drn-engineering.com>
2019-10-21 13:05:45 +0300

    add logger in placeController

--- src/main/java/com/example/ptuxiakh/controllers/GoOutPlaceController.java
+++ src/main/java/com/example/ptuxiakh/controllers/GoOutPlaceController.java
@@ -2,6 +2,8 @@ package com.example.ptuxiakh.controllers;
 
 import com.example.ptuxiakh.model.general.GoOutPlace;
 import com.example.ptuxiakh.services.GoOutPlaceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,14 @@ import javax.validation.constraints.NotNull;
 @RequestMapping("/api/places")
 public class GoOutPlaceController {
 
+    Logger logger = LoggerFactory.getLogger(GoOutPlaceController.class);
+
     @Autowired
     GoOutPlaceService goOutPlaceService;
 
     @GetMapping("/all")
     public ResponseEntity getAllPlaces(){
+        logger.debug("In Action getAllPlaces");
         try{
             return new ResponseEntity(goOutPlaceService.getAllPlaces(), HttpStatus.OK);
         }catch (Exception exc){
@@ -27,6 +32,8 @@ public class GoOutPlaceController {
 
     @GetMapping("/{placeId}")
     public ResponseEntity getSinglePlace(@NotNull @PathVariable String placeId){
+        logger.debug("In Action getSinglePlace with id: "+placeId);
+
         try{
             if (placeId != null)
                 return new ResponseEntity(goOutPlaceService.getSinglePlace(placeId), HttpStatus.OK);
@@ -38,7 +45,9 @@ public class GoOutPlaceController {
 
     @DeleteMapping("/delete/{placeId}")
     public ResponseEntity deleteSinglePlace(@NotNull @PathVariable String placeId){
-       try {
+        logger.debug("In Action deleteSinglePlace with id: "+placeId);
+
+        try {
            if (placeId != null) {
                 goOutPlaceService.deletePlace(placeId);
            }
@@ -50,6 +59,8 @@ public class GoOutPlaceController {
 
     @PostMapping("/new")
     public ResponseEntity createNewPlace(@RequestBody GoOutPlace goOutPlace){
+        logger.debug("In Action createNewPlace");
+
         try{
             if (goOutPlace != null) {
                 GoOutPlace result = goOutPlaceService.createNewPlace(goOutPlace);
@@ -63,6 +74,8 @@ public class GoOutPlaceController {
 
     @PutMapping("/update/{placeId}")
     public ResponseEntity updatePlace(@NotNull @PathVariable String placeId, @RequestBody GoOutPlace goOutPlace){
+        logger.debug("In Action updatePlace with id: "+placeId);
+
         try{
             if (placeId != null && goOutPlace != null) {
                 GoOutPlace result = goOutPlaceService.updatePlace(placeId, goOutPlace);
