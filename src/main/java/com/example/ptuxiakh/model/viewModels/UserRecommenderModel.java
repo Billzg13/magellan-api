12639062d1b4322a8fa25d7e909a3c0eb54b4a68
Billzg13 <Billzg1234@gmail.com>
2020-07-23 14:18:03 +0300

    add data for recommender and route for it

--- src/main/java/com/example/ptuxiakh/model/viewModels/UserRecommenderModel.java
+++ src/main/java/com/example/ptuxiakh/model/viewModels/UserRecommenderModel.java
@@ -1,13 +1,13 @@
 package com.example.ptuxiakh.model.viewModels;
 
-import com.example.ptuxiakh.model.Types;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-@Document(collection = "user")
+@Document(collection = "userRecommender")
 public class UserRecommenderModel {
     /*
     gender: 0 || male 0, female 1
@@ -23,10 +23,16 @@ cafe: 0 - 5
 health: 0 - 5
 gym: 0 - 5
      */
+
     @Id
     private String id;
 
+    private String userId;
+
+    @JsonIgnore
     String gender;
+
+    int genderNumeric;
 
     int age;
 
@@ -34,17 +40,61 @@ gym: 0 - 5
     @Max(5)
     int priceLevel; // 0 - 5
 
-    Types types;
+    @Min(0)
+    @Max(5)
+    int restaurant;
+
+    @Min(0)
+    @Max(5)
+    int lodging;
+
+    @Min(0)
+    @Max(5)
+    int food;
+
+    @Min(0)
+    @Max(5)
+    int pointOfInterest;
+
+    @Min(0)
+    @Max(5)
+    int establishment;
+
+    @Min(0)
+    @Max(5)
+    int bar;
+
+    @Min(0)
+    @Max(5)
+    int cafe;
+
+    @Min(0)
+    @Max(5)
+    int health;
+
+    @Min(0)
+    @Max(5)
+    int gym;
+
+    int placeId;
 
     public UserRecommenderModel() {
     }
 
-    public UserRecommenderModel(String id, String gender, int age, @Min(0) @Max(5) int priceLevel, Types types) {
-        this.id = id;
-        this.gender = gender;
-        this.age = age;
-        this.priceLevel = priceLevel;
-        this.types = types;
+    public int getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(int placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getId() {
@@ -61,6 +111,20 @@ gym: 0 - 5
 
     public void setGender(String gender) {
         this.gender = gender;
+        if (gender == "male")
+            this.genderNumeric = 0;
+        else if (gender == "female")
+            this.genderNumeric = 1;
+        else
+            this.genderNumeric = 2; //in case "other" is selected
+    }
+
+    public int getGenderNumeric() {
+        return genderNumeric;
+    }
+
+    public void setGenderNumeric(int genderNumeric) {
+        this.genderNumeric = genderNumeric;
     }
 
     public int getAge() {
@@ -79,11 +143,75 @@ gym: 0 - 5
         this.priceLevel = priceLevel;
     }
 
-    public Types getTypes() {
-        return types;
+    public int getRestaurant() {
+        return restaurant;
     }
 
-    public void setTypes(Types types) {
-        this.types = types;
+    public void setRestaurant(int restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public int getLodging() {
+        return lodging;
+    }
+
+    public void setLodging(int lodging) {
+        this.lodging = lodging;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public int getPointOfInterest() {
+        return pointOfInterest;
+    }
+
+    public void setPointOfInterest(int pointOfInterest) {
+        this.pointOfInterest = pointOfInterest;
+    }
+
+    public int getEstablishment() {
+        return establishment;
+    }
+
+    public void setEstablishment(int establishment) {
+        this.establishment = establishment;
+    }
+
+    public int getBar() {
+        return bar;
+    }
+
+    public void setBar(int bar) {
+        this.bar = bar;
+    }
+
+    public int getCafe() {
+        return cafe;
+    }
+
+    public void setCafe(int cafe) {
+        this.cafe = cafe;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getGym() {
+        return gym;
+    }
+
+    public void setGym(int gym) {
+        this.gym = gym;
     }
 }
