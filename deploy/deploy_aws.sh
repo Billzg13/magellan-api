e793e383d63320ee289f687ab4a4cd27afedcfd4
Billzg13 <Billzg1234@gmail.com>
2020-09-29 17:27:17 +0300

    change deploy

--- deploy/deploy_aws.sh
+++ deploy/deploy_aws.sh
@@ -2,7 +2,12 @@
 
 echo "in deploy_aws"
 #ssh magellan@18.157.184.133
-sshpass -p 'secret12345' ssh magellan@18.157.184.133 eval "$(ssh-agent -s)" ssh-add ~/.ssh/id_rsa rm -rf magellan-api git clone git@github.com:Billzg13/magellan-api.git
+sshpass -p 'secret12345' ssh magellan@18.157.184.133 <<EOF
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+rm -rf magellan-api
+git clone git@github.com:Billzg13/magellan-api.git
+EOF
 echo "succesfull ssh"
 
 eval "$(ssh-agent -s)"
