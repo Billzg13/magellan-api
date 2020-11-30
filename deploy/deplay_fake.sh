296ddf7fd9c4d324bfd5ed6a405e4d46144a764e
Vasilis Zgantzouris <36958390+Billzg13@users.noreply.github.com>
2020-11-30 17:31:30 +0200

    Update deplay_fake.sh

--- deploy/deplay_fake.sh
+++ deploy/deplay_fake.sh
@@ -4,7 +4,7 @@ echo "in deploy_aws"
 eval "$(ssh-agent -s)"
 ls
 
-ssh-add ./ptuxiakiKey.pem
+ssh-add ./deploy/ptuxiakiKey.pem
 
 ssh ubuntu@ec2-18-195-215-56.eu-central-1.compute.amazonaws.com
 echo "after successfull ssh ?"
