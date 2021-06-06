38c548560188a8a2d75a97f20a9b58bd8ef13e8f
Vasilis Zgantzouris <vassilis.zgantzouris@turbonomic.com>
2021-06-06 22:03:14 +0300

    Refactor routes

--- src/main/java/com/example/ptuxiakh/controllers/SearchController.java
+++ src/main/java/com/example/ptuxiakh/controllers/SearchController.java
@@ -8,7 +8,6 @@ import com.example.ptuxiakh.services.SearchService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,9 +26,9 @@ public class SearchController {
     JwtTokenProvider tokenProvider;
 
     @PostMapping("/quick")
-    public ResponseEntity quickSearch(HttpServletRequest request){
+    public ResponseEntity quickSearch(HttpServletRequest request) {
         logger.debug("In action: quickSearch");
-        try{
+        try {
             String token = request.getHeader("Authorization").substring(7);
             String userId = tokenProvider.extractUserIdFromJwt(token);
             QuickSearchResponse response = searchService.quickSearch(userId);
@@ -39,10 +38,10 @@ public class SearchController {
                     ),
                     HttpStatus.OK
             );
-        }catch (NullPointerException exc){
+        } catch (NullPointerException exc) {
             exc.printStackTrace();
             return new ResponseEntity(new ErrorResponse(exc.getMessage()), HttpStatus.BAD_REQUEST);
-        }catch (Exception exc) {
+        } catch (Exception exc) {
             exc.printStackTrace();
             return new ResponseEntity(new ErrorResponse(exc.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -59,16 +58,18 @@ public class SearchController {
     String category
     int price
      */
+
     /**
      * the new advanced search route
+     *
      * @param request
      * @param advancedSearch
      * @return
      */
     @PostMapping("/advanced")
-    public  ResponseEntity advancedSearch(HttpServletRequest request, @RequestBody com.example.ptuxiakh.model.SolidSearch.AdvancedSearchRequest advancedSearch){
-        logger.debug("In action: advancedSearch "+advancedSearch.toString());
-        try{
+    public ResponseEntity advancedSearch(HttpServletRequest request, @RequestBody com.example.ptuxiakh.model.SolidSearch.AdvancedSearchRequest advancedSearch) {
+        logger.debug("In action: advancedSearch " + advancedSearch.toString());
+        try {
             if (advancedSearch == null)
                 return new ResponseEntity(new Error("no search provided"), HttpStatus.BAD_REQUEST);
             String token = request.getHeader("Authorization").substring(7);
@@ -78,40 +79,37 @@ public class SearchController {
             String searchIdResult = searchService.advancedSearch(userId, advancedSearch, searchId);
             return new ResponseEntity<>(new QuickSearchResult(searchIdResult), HttpStatus.OK);
 
-        }catch (Exception exc){
+        } catch (Exception exc) {
             exc.printStackTrace();
             return new ResponseEntity(new Error(exc.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/quick/user")
-    public ResponseEntity getLatestQuickSearches(HttpServletRequest request){
+    @GetMapping("/history")
+    public ResponseEntity getSearchHistory(HttpServletRequest request) {
         logger.debug("In action: quickSearch");
-        try{
+        try {
             String token = request.getHeader("Authorization").substring(7);
             String userId = tokenProvider.extractUserIdFromJwt(token);
             return ResponseEntity.ok(searchService.getLatestQuickSearches(userId));
-        }catch (NullPointerException exc){
+        } catch (NullPointerException exc) {
             exc.printStackTrace();
             return new ResponseEntity(new ErrorResponse(exc.getMessage()), HttpStatus.BAD_REQUEST);
-        }catch (Exception exc) {
+        } catch (Exception exc) {
             return new ResponseEntity(new ErrorResponse(exc.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/quick/search")
-    public ResponseEntity getLatestQuickSearches(HttpServletRequest request, @RequestParam String searchId){
+    @GetMapping("/")
+    public ResponseEntity getSearch(HttpServletRequest request, @RequestParam String recId) {
         logger.debug("In action: quickSearch");
-        try{
-            return ResponseEntity.ok(searchService.findQuickSearchById(searchId));
-        }catch (NullPointerException exc){
+        try {
+            return ResponseEntity.ok(searchService.findQuickSearchById(recId));
+        } catch (NullPointerException exc) {
             exc.printStackTrace();
             return new ResponseEntity(new ErrorResponse(exc.getMessage()), HttpStatus.BAD_REQUEST);
-        }catch (Exception exc) {
+        } catch (Exception exc) {
             return new ResponseEntity(new ErrorResponse(exc.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-
-
 }
